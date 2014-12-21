3b594c456ac5f3037e5574da988508d9740f5d2a
Chris Tram <chris.tram@gmail.com>
2014-12-20 16:32:26 -0800

    Add tests and views for microposts at root page

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,15 @@
+class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
+    
+end
